9589ce2062cfd9fb052af8ac6696dac76afa6db7
anne_yan <anne_yan@infosys.com>
2016-11-23 17:24:41 +0800

    a

--- westingweixinApp/westingweixin/src/main/java/com/infosys/dao/ServiceOrderDao.java
+++ westingweixinApp/westingweixin/src/main/java/com/infosys/dao/ServiceOrderDao.java
@@ -1,10 +1,12 @@
 package com.infosys.dao;
 
 
+import org.springframework.stereotype.Repository;
+
 import com.infosys.dao.base.BaseDao;
 import com.infosys.entity.ServiceOrder;
 
-//@Repository("serviceOrderDao")
+@Repository("serviceOrderDao")
 public class ServiceOrderDao extends BaseDao<ServiceOrder> implements IServiceOrderDao {
 
 
