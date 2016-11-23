acd55bf0ae6111238bd92da7ec643cdd49cd4bd6
anne_yan <anne_yan@infosys.com>
2016-11-23 15:50:07 +0800

    baseDao

---
+++ westingweixinApp/westingweixin/src/main/java/com/infosys/dao/ServiceOrderDao.java
@@ -0,0 +1,11 @@
+package com.infosys.dao;
+
+
+import com.infosys.dao.base.BaseDao;
+import com.infosys.entity.ServiceOrder;
+
+//@Repository("serviceOrderDao")
+public class ServiceOrderDao extends BaseDao<ServiceOrder> implements IServiceOrderDao {
+
+
+}
