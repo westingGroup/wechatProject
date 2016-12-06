934d28a110854f623fc34e18186c05534a65afbe
lily_xue123 <394184669@qq.com>
2016-12-06 10:29:03 +0800

    测试

--- westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/IndexController.java
+++ westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/IndexController.java
@@ -10,7 +10,35 @@ public class IndexController {
 	@RequestMapping(value = "/index")
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("index");
+		modelAndView.setViewName("process/registers/approvalRegister");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/dispatchRegister")
+	public ModelAndView dispatchRegister() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("register/dispatchRegister");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/ordersRegister")
+	public ModelAndView ordersRegister() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("register/ordersRegister");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/demanderView")
+	public ModelAndView demanderView() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("demander/view");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/approvalDemand")
+	public ModelAndView approvalDemand() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("process/demanders/approvalDemander");
 		return modelAndView;
 	}
 }
