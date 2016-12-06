d9bdcdbf217e8265d7ebed435c4a77281d4df574
lily_xue123 <394184669@qq.com>
2016-12-06 14:31:03 +0800

    服务端页面

--- westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/ProviderController.java
+++ westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/ProviderController.java
@@ -22,85 +22,93 @@ import com.infosys.basic.service.IApplyService;
 import com.infosys.basic.service.IProviderService;
 import com.infosys.basic.service.IServiceOrderService;
 import com.infosys.basic.util.Constants;
+import com.infosys.basic.util.DateUtil;
 
 //服务提供方
 @RequestMapping("/provider")
 @Controller
 public class ProviderController {
-    @Inject
-    private IServiceOrderService serviceOrderService;
+	@Inject
+	private IServiceOrderService serviceOrderService;
 
-    @Inject
-    private IProviderService providerService;
+	@Inject
+	private IProviderService providerService;
 
-    @Inject
-    private IApplyService applyService;
-    
+	@Inject
+	private IApplyService applyService;
 
-    // 我的服务
-    @RequestMapping("/list")
-    public String list(HttpSession session, Model model) {
-        User u = (User) session.getAttribute(Constants.WEIXIN_SESSION_USER);
-        Provider provider = providerService.loadByOpenid(u.getOpenid());
-        if (provider == null) {
-            model.addAttribute("provider", new Provider());
-            // 进入服务供应商-注册页面
-            model.addAttribute("fromPath", "2");
-            model.addAttribute("openid", u.getOpenid());
-            return "provider/register";
-        }
-        // 进入服务供应商-我的服务 (未申领；别人已申领待分配)
-        List<ServiceOrderDto> orders = serviceOrderService.listProviderServiceOrder(u.getOpenid());
-        model.addAttribute("orders", orders);
-        return "provider/list";
-    }
+	// 我的服务
+	@RequestMapping("/list")
+	public String list(HttpSession session, Model model) {
+		User u = (User) session.getAttribute(Constants.WEIXIN_SESSION_USER);
+		Provider provider = providerService.loadByOpenid(u.getOpenid());
+		if (provider == null) {
+			model.addAttribute("provider", new Provider());
+			// 进入服务供应商-注册页面
+			model.addAttribute("fromPath", "2");
+			model.addAttribute("openid", u.getOpenid());
+			return "provider/register";
+		}
+		// 进入服务供应商-我的服务 (未申领；别人已申领待分配)
+		List<ServiceOrderDto> orders = serviceOrderService
+				.listProviderServiceOrder(u.getOpenid());
+		model.addAttribute("orders", orders);
+		return "provider/list";
+	}
 
-    // 服务认领
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add(HttpSession session, Model model) {
-        User u = (User) session.getAttribute(Constants.WEIXIN_SESSION_USER);
-        Provider provider = providerService.loadByOpenid(u.getOpenid());
-        if (provider == null) {
-            model.addAttribute("provider", new Provider());
-            // 进入服务供应商-注册页面
-            model.addAttribute("fromPath", "1");
-            model.addAttribute("openid", u.getOpenid());
-            return "provider/register";
-        }
-        // 进入服务供应商-认领页面
-        List<ServiceOrderDto> orders = serviceOrderService.listProviderServiceOrder(u.getOpenid());
-        model.addAttribute("orders", orders);
-        model.addAttribute("apply", new Apply());
-        model.addAttribute("provider", provider);
-        return "provider/add";
-    }
+	// 服务认领
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(HttpSession session, Model model) {
+		User u = (User) session.getAttribute(Constants.WEIXIN_SESSION_USER);
+		Provider provider = providerService.loadByOpenid(u.getOpenid());
+		if (provider == null) {
+			model.addAttribute("provider", new Provider());
+			// 进入服务供应商-注册页面
+			model.addAttribute("fromPath", "1");
+			model.addAttribute("openid", u.getOpenid());
+			return "provider/register";
+		}
+		// 进入服务供应商-认领页面
+		List<ServiceOrderDto> orders = serviceOrderService
+				.listProviderServiceOrder(u.getOpenid());
+		model.addAttribute("orders", orders);
+		model.addAttribute("apply", new Apply());
+		model.addAttribute("provider", provider);
+		return "provider/add";
+	}
 
-    // 认领
-    @RequestMapping(value = "/add/{id}", method = RequestMethod.POST)
-    @ResponseBody
-    public String add(@PathVariable int id,Apply apply,@RequestParam(value = "providerId", required = false) String providerId,
-            @RequestParam(value = "providerName", required = false) String providerName) {
-        apply.setApplyBy(Integer.parseInt(providerId));//外部人员
-        apply.setApplyname(providerName);
-        apply.setApplyDate(new Date());
-        apply.setsId(id);
-        applyService.add(apply);
-        return "您已经认领成功";
-    }
+	// 认领
+	@RequestMapping(value = "/add/{id}", method = RequestMethod.POST)
+	@ResponseBody
+	public String add(
+			@PathVariable int id,
+			Apply apply,
+			@RequestParam(value = "providerId", required = false) String providerId,
+			@RequestParam(value = "providerName", required = false) String providerName) {
+		apply.setApplyBy(Integer.parseInt(providerId));// 外部人员
+		apply.setApplyname(providerName);
+		apply.setApplyDate(new Date());
+		apply.setsId(id);
+		applyService.add(apply);
+		return "您已经认领成功";
+	}
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(@RequestParam(value = "fromPath", required = false) String fromPath,
-            @RequestParam(value = "openid", required = false) String openid, Provider provider, HttpSession session,
-            Model model) {
-        provider.setOpenid(openid);
-        provider.setStatus(1);
-        providerService.add(provider);
-        if (fromPath.equals("1")) {
-            return "redirect:/provider/add";
-        } else if (fromPath.equals("2")) {
-            return "redirect:/provider/list";
-        }
-        return "redirect:/provider/add";
-    }
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(
+			@RequestParam(value = "fromPath", required = false) String fromPath,
+			@RequestParam(value = "birth", required = false) String birth,
+			@RequestParam(value = "openid", required = false) String openid,
+			Provider provider, HttpSession session, Model model) {
+		provider.setOpenid(openid);
+		provider.setStatus(1);
+		provider.setBirthDate(DateUtil.parseDate(birth, "yyyy-MM-dd"));
+		providerService.add(provider);
+		if (fromPath.equals("1")) {
+			return "redirect:/provider/add";
+		} else if (fromPath.equals("2")) {
+			return "redirect:/provider/list";
+		}
+		return "redirect:/provider/add";
+	}
 
 }
--- westingweixinApp/westingweixin/src/main/webapp/WEB-INF/pages/provider/register.jsp
+++ westingweixinApp/westingweixin/src/main/webapp/WEB-INF/pages/provider/register.jsp
@@ -20,13 +20,28 @@
 <link rel="stylesheet" type="text/css"
 	href="<%=request.getContextPath()%>/assets/media/css/style.css" />
 <link rel="stylesheet" type="text/css"
+	href="<%=request.getContextPath()%>/assets/media/css/datetimepicker.css" />
+<link rel="stylesheet" type="text/css"
+	href="<%=request.getContextPath()%>/assets/media/css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css"
 	href="<%=request.getContextPath()%>/assets/css/common/common.css" />
 <link rel="stylesheet" type="text/css"
 	href="<%=request.getContextPath()%>/assets/css/common/common_service.css" />
 <script type="text/javascript"
 	src="<%=request.getContextPath()%>/assets/js/jquery-1.8.2.min.js"></script>
 <script type="text/javascript"
+	src="<%=request.getContextPath()%>/assets/media/js/bootstrap.min.js"></script>
+<script type="text/javascript"
+	src="<%=request.getContextPath()%>/assets/media/js/bootstrap-datetimepicker.js"></script>
+<script type="text/javascript"
+	src="<%=request.getContextPath()%>/assets/media/js/bootstrap-datetimepicker.zh-CN.js"></script>
+<script type="text/javascript"
 	src="<%=request.getContextPath()%>/assets/js/common/common.js"></script>
+<script type="text/javascript">
+	$(function() {
+		initDatePickerForDay();
+	});
+</script>
 </head>
 <body>
 	<div class="subject">
@@ -48,58 +63,81 @@
 		<sf:form modelAttribute="provider" id="adminForm" method="post"
 			action="/provider/register">
 			<sf:hidden path="id" />
-			<input type="hidden" name="fromPath" id="fromPath" value="${fromPath}"></input>
+			<input type="hidden" name="fromPath" id="fromPath"
+				value="${fromPath}"></input>
+			<input type="hidden" name="openid" id="openid" value="${openid}"></input>
 			<div class="content">
 				<div class="container commonStyle contact">
 					<div class="row contactPerson">
-						<div class="col-md-1 col-xs-3 label">
+						<div class="col-md-1 col-xs-4 label">
 							<img alt="姓名"
 								src="<%=request.getContextPath()%>/assets/img/contactPerson.png"
 								width="16px" height="16px">&nbsp;<font color="red">*</font>&nbsp;姓名：
 						</div>
-						<div class="col-md-11 col-xs-9">
+						<div class="col-md-11 col-xs-8">
 							<sf:input path="linkname" cssClass="text" />
 						</div>
 					</div>
 					<hr class="commonHr" />
 					<div class="row contactPhone">
-						<div class="col-md-1 col-xs-3 label">
+						<div class="col-md-1 col-xs-4 label">
 							<img alt="电话"
 								src="<%=request.getContextPath()%>/assets/img/contactPhone.png"
 								width="16px" height="16px">&nbsp;<font color="red">*</font>&nbsp;电话：
 						</div>
-						<div class="col-md-11 col-xs-9">
+						<div class="col-md-11 col-xs-8">
 							<sf:input path="linkphone" cssClass="text" />
+						</div>
+					</div>
+					<hr class="commonHr" />
+					<div class="row contactBirthday">
+						<div class="col-md-1 col-xs-4 label">
+							<img alt="出生年月"
+								src="<%=request.getContextPath()%>/assets/img/calendar.png"
+								width="16px" height="16px">&nbsp;&nbsp;出生年月：
+						</div>
+						<div
+							class="col-md-11 col-xs-8 input-append date form_datetime_day"
+							style="padding: 0px;">
+							<div class="row">
+								<div class="col-md-10 col-xs-7" style="padding: 0px;">
+									<input type="text" class="text" name="birth" />
+								</div>
+								<div class="col-md-2 col-xs-5"
+									style="padding: 0px; text-align: right;">
+									<span class="add-on"><i class="icon-remove"></i></span> <span
+										class="add-on"><i class="icon-calendar"></i></span>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="container commonStyle company">
 					<div class="row companyBusiness">
-						<div class="col-md-1 col-xs-3 label">
+						<div class="col-md-1 col-xs-4 label">
 							&nbsp;<font color="red" style="margin-left: 16px;">*</font>&nbsp;擅长业务：
 						</div>
-						<div class="col-md-11 col-xs-9">
+						<div class="col-md-11 col-xs-8">
 							<sf:input path="business" cssClass="text" />
 						</div>
 					</div>
 					<hr class="commonHr" />
 					<div class="row companyQualifition">
-						<div class="col-md-1 col-xs-3 label">
-							&nbsp;<font color="red" style="margin-left: 16px;">*</font>&nbsp;相关资质：
+						<div class="col-md-1 col-xs-4 label">
+							<span style="margin-left: 26px;"></span>职称/资质：
 						</div>
-						<div class="col-md-11 col-xs-9">
+						<div class="col-md-11 col-xs-8">
 							<sf:input path="qualification" cssClass="text" />
 						</div>
 					</div>
 					<hr class="commonHr" />
 					<div class="row companyName">
-						<div class="col-md-1 col-xs-3 label">
+						<div class="col-md-1 col-xs-4 label">
 							<span style="margin-left: 26px;">公司名称：</span>
 						</div>
-						<div class="col-md-11 col-xs-9">
-							<sf:textarea path="company" rows="3" cssClass="textarea" />
+						<div class="col-md-11 col-xs-8">
+							<sf:input path="company" cssClass="text" />
 						</div>
-
 					</div>
 				</div>
 			</div>
