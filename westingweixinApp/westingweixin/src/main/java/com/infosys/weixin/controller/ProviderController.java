fde90926ff405834d7df9dd8529b0d6314f97730
anne_yan <anne_yan@infosys.com>
2016-12-15 17:34:49 +0800

    他他

--- westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/ProviderController.java
+++ westingweixinApp/westingweixin/src/main/java/com/infosys/weixin/controller/ProviderController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.infosys.basic.dto.PagerInfo;
 import com.infosys.basic.dto.ServiceOrderDto;
